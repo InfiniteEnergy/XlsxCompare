7fb18379520990484749ceff64e16d78d1e34dba
Ryan Davis <rdavis@infiniteenergy.com>
2021-04-01 15:14:03 -0400

    add facade around EPPlus
    get some simpler operations we want for comparison, normalizing some common string bits we don't care about.

--- src/XlsxCompare/XlsxFacade.cs
+++ src/XlsxCompare/XlsxFacade.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using OfficeOpenXml;
 
 namespace XlsxCompare
@@ -11,14 +13,73 @@ namespace XlsxCompare
     sealed class XlsxFacade : IDisposable
     {
         readonly ExcelPackage _excel;
+        readonly IReadOnlyDictionary<string, int> _columnMap;
 
-        public XlsxFacade(string path) : this(new FileInfo(path)) { }
+        private ExcelWorksheet Sheet => _excel.Workbook.Worksheets[0];
 
-        public XlsxFacade(FileInfo file)
+        private XlsxFacade(FileInfo file)
         {
             _excel = new ExcelPackage(file);
+            _columnMap = BuildColumnMap(Sheet);
+        }
+
+        /// <summary>
+        /// converts everything to trimmed strings, normalizing `null`s, "NULL"s,
+        /// and "" to ""
+        /// </summary>
+        public string GetSafeValue(int row, string columnName)
+        {
+            var col = _columnMap[columnName];
+            var value = Sheet.Cells[row, col].Value;
+            return Normalize(value?.ToString());
+        }
+
+        private static string Normalize(string? value)
+            => value switch
+            {
+                "NULL" => "",
+                null => "",
+                _ => value.Trim()
+            };
+
+        public int FindRow(string columnName, string value)
+        {
+            foreach (var row in Enumerable.Range(2, Sheet.Dimension.Rows))
+            {
+                var candidate = GetSafeValue(row, columnName);
+                if (value.Equals(candidate, StringComparison.OrdinalIgnoreCase))
+                {
+                    return row;
+                }
+            }
+            throw new KeyNotFoundException($"Could not find '{value}' in {columnName}");
         }
 
         public void Dispose() => _excel.Dispose();
+
+        public override string ToString()
+        {
+            return $"[{GetType().Name} {Sheet.Name}, {Sheet.Dimension}]";
+        }
+
+        public static XlsxFacade Open(string path)
+        {
+            var file = new FileInfo(path);
+            if (!file.Exists) { throw new FileNotFoundException("Could not open xlsx", file.FullName); }
+            return new XlsxFacade(file);
+        }
+
+        static IReadOnlyDictionary<string, int> BuildColumnMap(ExcelWorksheet sheet)
+            => Enumerable.Range(1, sheet.Dimension.Columns)
+                .Select(index => new
+                {
+                    index,
+                    header = sheet.Cells[1, index].Value?.ToString()
+                })
+                .Where(x => x.header != null)
+                .ToDictionary(
+                    x => x.header!.Trim(),
+                    x => x.index,
+                    StringComparer.OrdinalIgnoreCase);
     }
 }
